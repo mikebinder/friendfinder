3b43a9de690706c94d6ee487b07ee0a7b9c71a45
Mike Binder <mikebinder@HAL9000.local>
2018-03-16 18:34:10 -0500

    working modal

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+var friends = [
+  {
+      "name":"Colonel John Matrix",
+      "photo":"http://www.writeups.org/wp-content/uploads/John-Matrix-Commando-Schwarzenegger-l.jpg",
+      "scores":[
+         5,
+         1,
+         4,
+         4,
+         5,
+         1,
+         2,
+         5,
+         4,
+         1
+          ]
+      },
+  {
+  "name":"T-800 Terminator",
+  "photo":"http://i.dailymail.co.uk/i/pix/2012/04/08/article-2126916-04F4994E000005DC-475_468x581.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+      ]
+},
+{
+  "name":"'Dutch'",
+  "photo":"http://www.writeups.org/wp-content/uploads/Dutch-Schaefer-Predator-Schwarzenegger-a.jpg",
+  "scores":[
+     3,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+      ]
+},
+{
+  "name":"Detective John Kimble",
+  "photo":"http://artistviewblog.com/wp-content/uploads/2015/03/Arnold-Kindergarten-cop.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     4,
+     4,
+     4,
+     2,
+     1
+      ]
+},
+{
+  "name":"John 'The Eraser' Kruger",
+  "photo":"https://vignette.wikia.nocookie.net/die-hard-scenario/images/3/3b/John_Kruger_in_Eraser_%281996%29.jpg/revision/latest?cb=20150704182902",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     4,
+     1,
+     2,
+     3,
+     5
+      ]
+},
+{
+  "name":"Douglas Quaid",
+  "photo":"https://pbs.twimg.com/profile_images/2998914948/ebbce712d5a0142f9bfc314b360b120e.jpeg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     2,
+     5,
+     1,
+     3,
+     2
+      ]
+},
+{
+  "name":"Ben Richards",
+  "photo":"https://cdn.instructables.com/FP8/YPQ4/H7ZPBXJ6/FP8YPQ4H7ZPBXJ6.LARGE.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     4,
+     1,
+     1,
+     1,
+     1
+      ]
+},
+{
+  "name":"Harry Tasker",
+  "photo":"https://static.comicvine.com/uploads/original/2/24850/599455-harry_tasker.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     5,
+     5,
+     5,
+     5,
+     5
+      ]
+},
+{
+  "name":"Conan the Barbarian",
+  "photo":"http://www.kultofathena.com/images/conan.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     2,
+     3,
+     3,
+     1,
+     5
+      ]
+},
+{
+  "name":"Jericho Cane",
+  "photo":"https://cdn.moviestillsdb.com/i/500x/awurtjoo/end-of-days-lg.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     2,
+     1,
+     5,
+     2,
+     4
+      ]
+}
+]
+
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,57 @@
+var friends = require("../data/friends");
+var path = require("path");
+
+
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+
+    app.post("/api/friends", function (req, res) {
+
+
+        var newFriend = req.body;
+        var newScore = newFriend.scores;
+        var total = 0;
+        var bestMatch = 1000;
+        var index = -1;
+
+        for (var i = 0; i < friends.length; i++) {
+
+            total = 0;
+
+            for (var j = 0; j < newScore.length; j++) {
+
+                var diff = Math.abs(newScore[j] - friends[i].scores[j]);
+                total += diff;
+            }
+            if (total < bestMatch) {
+                bestMatch = total;
+                index = i;
+            }
+        }
+        
+        console.log('Best Match:', friends[index]);
+
+        var bestie = {
+            name: friends[index].name,
+            photo: friends[index].photo
+        }
+
+console.log(bestie.name)
+console.log(bestie.photo)
+
+        friends.push(newFriend);
+        res.json(bestie);
+
+        
+    });
+};
+
+
+
+
+
+
